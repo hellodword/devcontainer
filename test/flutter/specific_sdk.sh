fd587dcea048231d02d94f9ef3f99c17bd99cb1a
hellodword <46193371+hellodword@users.noreply.github.com>
2024-02-13 07:10:47 +0000

    flutter: add extension and build tests

--- test/flutter/specific_sdk.sh
+++ test/flutter/specific_sdk.sh
@@ -6,5 +6,6 @@ source dev-container-features-test-lib
 check "execute command" bash -c "id; whoami; env"
 check "execute command" bash -c "ls -alh $FLUTTER_HOME"
 check "execute command" bash -c "flutter doctor -v"
+check "execute command" bash -c "cd /tmp && flutter create test_drive && cd test_drive && flutter build linux && flutter build apk"
 
 reportResults
