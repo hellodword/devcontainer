58c261bbeaa73b1a83437add17886c99b1e8cd61
hellodword <46193371+hellodword@users.noreply.github.com>
2024-02-15 06:29:15 +0000

    fix: common replace apt with apt-get

--- src/common/install.sh
+++ src/common/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 set +H
+set -x
 
-DEBIAN_FRONTEND="noninteractive" sudo apt update &&
-    sudo apt install --no-install-recommends -y \
-        jq file vim bash-completion
+DEBIAN_FRONTEND="noninteractive" sudo apt-get update &&
+    sudo apt-get install --no-install-recommends -y \
+        jq file vim bash-completion &&
+    sudo apt-get clean
