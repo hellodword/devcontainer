41a13423589e216ae417a7546adb98e9fc3473f9
hellodword <46193371+hellodword@users.noreply.github.com>
2024-03-27 05:04:55 +0000

    fix(features): solana fix PATH

--- features/src/solana/install.sh
+++ features/src/solana/install.sh
@@ -5,7 +5,12 @@ set -x
 
 # avoid using github api which has rate limit, especially with VPN
 REPO="solana-labs/solana"
-LATEST_VERSION="$(curl -fsS -w "%{redirect_url}" -o /dev/null "https://github.com/$REPO/releases/latest" | grep -oP '(?<=/releases/tag/)[^/]+$')"
+export LATEST_VERSION="$(curl -fsS -w "%{redirect_url}" -o /dev/null "https://github.com/$REPO/releases/latest" | grep -oP '(?<=/releases/tag/)[^/]+$')"
 [ -n "$LATEST_VERSION" ]
 
-sh -c "$(curl -sSfL https://release.solana.com/$LATEST_VERSION/install)"
+sudo -Eu "$_REMOTE_USER" bash -ex << EOF
+# fix HOME rewrited by sudo -E
+export HOME="$_REMOTE_USER_HOME"
+
+curl -sSfL "https://release.solana.com/$LATEST_VERSION/install" | sh
+EOF
